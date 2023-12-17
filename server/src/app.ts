4ea108f7775e2c7259c973bd0238d68e4da8818e
Dev <flashytesting@gmail.com>
2023-12-17 21:51:49 +0530

    Add socket.io on app server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,33 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (_req, res) => {
     const response = { message: "Hello World!" };
     res.send(response);
 })
 
-app.listen(PORT, () => {
+io.on('connection', socket => {
+    console.log(`${socket.id} connected`);
+
+    socket.on('chatMessage', message => {
+        console.log(message);
+    })
+    
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} disconnected`);
+    })
+})
+
+server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 })
 
