420e2fbc206c609c9c635e39ba533c1483efefb7
Dev <flashytesting@gmail.com>
2023-12-17 23:17:03 +0530

    Receive broadcasted message on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,71 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import MessageBubble from "./components/MessageBubble";
 import MessageForm from "./components/MessageForm";
+import { socket } from "./socket";
+
+type MessageData = {
+    username: string;
+    message: string;
+}
 
 const App = () => {
+    const [username, setUsername] = useState('');
     const [message, setMessage] = useState('');
-    const [messageLogs, setMessageLogs] = useState<string[]>([]);
+    const [messageDataLogs, setMessageDataLogs] = useState<MessageData[] | null>(null);
+    const [isConnected, setIsConnected] = useState(socket.connected);
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    useEffect(() => {
+        console.log('here');
+
+        const onConnect = () => {
+            console.log('inside connect');
+            setIsConnected(true)
+        }
+
+        const onDisconnect = () => {
+            console.log('inside disconnect');
+            setIsConnected(false)
+        }
+
+        const onChatMessage = (messageData: MessageData) => {
+            console.log('inside chatMessage')
+            setMessageDataLogs(prevLogs => {
+                return prevLogs
+                    ? [...prevLogs, messageData]
+                    : [messageData]
+            })
+        }
+
+        socket.on('connect', onConnect);
+        socket.on('disconnect', onDisconnect);
+        socket.on('chatMessage', onChatMessage);
+
+        return () => {
+            socket.off('connect');
+            socket.off('disconnect');
+            socket.off('chatMessage');
+        }
+    }, []);
+
+    const handleUsernameSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        console.log(username);
+        socket.connect();
+    }
+
+    const handleMessageSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         // TODO: BrodcastMessage to all clients
-        console.log(message);
-        setMessageLogs(prevMessageLog => {
-            return [...prevMessageLog, message];
-        })
+        const messageData: MessageData = {
+            username,
+            message
+        }
+
+        // Sending message from client
+        console.log('here in client before', messageData);
+        socket.emit('chatMessage', messageData);
 
         setMessage('');
     }
@@ -21,17 +73,33 @@ const App = () => {
     return (
         <div className='relative h-screen'>
             {/* FIX: Proper scroll on message */}
-            <div className='p-5 flex flex-col gap-3'>
-                {messageLogs.map((message, index) => (
-                    <MessageBubble key={index} message={message} />
-                ))}
-            </div>
+            {!isConnected && (
+                <form onSubmit={handleUsernameSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Enter your name"
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-gray-800 text-sm px-3 py-2 color-white outline-none"
+                    />
+                </form>
+            )}
 
-            <MessageForm
-                message={message}
-                setMessage={setMessage}
-                handleSubmit={handleSubmit}
-            />
+            {isConnected && (
+                <>
+                    <div className='p-5 flex flex-col gap-3'>
+                        {messageDataLogs && messageDataLogs.map((messageData, index) => (
+                            <MessageBubble key={index} messageData={messageData} username={username} />
+                        ))}
+                    </div>
+
+                    <MessageForm
+                        message={message}
+                        setMessage={setMessage}
+                        handleMessageSubmit={handleMessageSubmit}
+                    />
+                </>
+            )}
         </div>
     )
 }
