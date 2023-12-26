e5bb57e2cb7ecc6df97060896987ec7419bc98b2
Dev <flashytesting@gmail.com>
2023-12-26 19:13:44 +0530

    Fix username display on chat section

--- client/src/components/MessageBubble.tsx
+++ client/src/components/MessageBubble.tsx
@@ -12,6 +12,27 @@ const MessageBubble = ({ messageData, messageDataLogs, username }: MessageBubble
     const currentMessageIndex = messageDataLogs.indexOf(messageData);
     const previousMessage = messageDataLogs[currentMessageIndex - 1];
 
+    // Username Preview (when NOT to show)
+    // - If primary user (isUser)
+    // - If NOT primary user then:
+    //     - if message username same as previous message username 
+    //     - And message type NOT same as previous message type
+
+    // const noUsername = isUser
+    //     ? false
+    //     : (
+    //         messageData.username === previousMessage.username &&
+    //         messageData.type === previousMessage.type
+    //             ? false
+    //             : true
+    //     )
+
+    const showUsername = !isUser &&
+        !(
+            messageData.username === previousMessage.username &&
+            messageData.type === previousMessage.type
+        );
+
     return (
         <>
             {messageData.type === 'message-log' && (
@@ -22,13 +43,9 @@ const MessageBubble = ({ messageData, messageDataLogs, username }: MessageBubble
             )}
 
             {messageData.type === 'message-bubble' && (
-                <div className={`relative px-3 py-2 rounded-3xl max-w-lg ${isUser ? 'self-end bg-[#0084FF] ml-12' : 'bg-gray-800 mr-12 mt-4'} w-fit text-sm`}>
-                    {/* TODO: Multiple message from same person should not have this */}
-                    {
-                        !isUser
-                        && previousMessage.username !== messageData.username
-                        && <h1 className='absolute text-[0.7rem] text-gray-500 bottom-[90%] left-[0.5rem] mb-1'>{messageData.username}</h1>
-                    }
+                <div className={`relative px-3 py-2 rounded-3xl max-w-lg ${showUsername && 'mt-4'} ${isUser ? 'self-end bg-[#0084FF] ml-12' : 'bg-gray-800 mr-12'} w-fit text-sm`}>
+
+                    {showUsername && <h1 className='absolute text-[0.7rem] text-gray-500 bottom-[90%] left-[0.5rem] mb-1'>{messageData.username}</h1>}
                     {messageData.message}
                 </div>
             )}
